f6b3ad905301b550877d36d0aa35067ea52f7f52
Marco Levrero <marco.levrero@gmail.com>
2018-11-03 18:23:30 +0000

    List exercise 20

---
+++ Lists/20.hs
@@ -0,0 +1,12 @@
+{- 
+  Remove the K'th element from a list.
+  
+  removeAt 2 "abcd" -> "acd"
+-}
+
+removeAt :: Int -> [a] -> [a]
+
+removeAt _ [] = []
+removeAt n xs
+  | n < 0 = error "n must be a positive integer"
+  | otherwise = take (n - 1) xs ++ drop n xs
