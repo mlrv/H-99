5974f709d66fc109b0a3637a0668ecb49acc8c6d
Marco Levrero <marco.levrero@gmail.com>
2018-11-05 15:12:42 +0000

    List exercise 18

---
+++ Lists/18.hs
@@ -0,0 +1,15 @@
+{- 
+  Extract a slice from a list.
+  Given two indices, i and k, the slice is the list containing the elements
+  between the i'th and k'th element of the original list (both limits included). Start counting the elements with 1.
+
+  slice [1, 2, 3, 4, 5, 6] 2 4 -> [2, 3, 4]
+-}
+
+slice :: [a] -> Int -> Int -> [a]
+
+slice [] _ _ = []
+slice xs j k
+  | j >= k = error "the first index must be smaller than the second one"
+  | otherwise = drop (j - 1) (take k xs)
+
