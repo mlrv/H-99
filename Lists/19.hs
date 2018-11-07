37e8c871701cac1379468302780c722ccee0ef62
Marco Levrero <marco.levrero@gmail.com>
2018-11-07 14:52:27 +0000

    List exercise 19

---
+++ Lists/19.hs
@@ -0,0 +1,13 @@
+{- 
+  Rotate a list N places to the left.
+  
+  rotate "abcdefgh" 3 -> "defghabc"
+-}
+
+rotate :: [a] -> Int -> [a]
+
+rotate [] _ = []
+rotate xs n
+  | n < 0 = error "n must be positive"
+  | n > length xs = rotate xs (n - length xs)
+  | otherwise = (drop n xs) ++ (take n xs)
