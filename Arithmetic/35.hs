647aa2315d74d65c3f942def97b71b853c6bffb0
Marco Levrero <marco.levrero@gmail.com>
2018-11-09 19:16:32 +0000

    Arithmetic exercise 35

---
+++ Arithmetic/35.hs
@@ -0,0 +1,19 @@
+{- 
+  Determine the prime factors of a given positive integer. 
+  
+  primeFactors 315 -> [3, 3, 5, 7]
+-}
+
+primeFactors :: Int -> [Int]
+
+primeFactors 1 = []
+primeFactors x =  pF : (primeFactors $ x `div` pF) where
+  pF = maximum [n | n <- [2..x], x `mod` n == 0, isPrime n]
+
+isPrime :: Int -> Bool
+
+isPrime x
+  | x <= 0 = False
+  | otherwise = not $ any hasDivisors [2..(x -1)]
+  where hasDivisors = \n -> x `mod` n == 0
+
