cf7b6dd9ca8f8d80a7f5e274ec056e0340b82c78
Marco Levrero <marco.levrero@gmail.com>
2018-12-06 19:15:44 +0000

    Init BT ex 56

---
+++ Binary Trees/56.hs
@@ -0,0 +1,13 @@
+{- 
+  Let us call a binary tree symmetric if you can draw a vertical line through
+  the root node and then the right subtree is the mirror image of the left subtree. 
+  
+  Write a predicate symmetric/1 to check whether a given binary tree is symmetric.
+
+  symmetric (Branch 'x' (Branch 'x' Empty Empty) Empty) -> False
+  symmetric (Branch 'x' (Branch 'x' Empty Empty) (Branch 'x' Empty Empty)) -> True
+-}
+
+data Tree a = Empty | Branch a (Tree a) (Tree a)
+              deriving (Show, Eq)
+
